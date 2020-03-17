e3f088f349f9703daa771478b2916401c412dead
marianolc <mc@dlocorp.com>
2020-03-17 16:30:11 -0600

    mlc: fixes

--- src/components/crud/Customers.js
+++ src/components/crud/Customers.js
@@ -1,72 +1,52 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import { customers, customersFiltered, deleteCustomer } from "../../actions";
 import TableContainer from "../shared/TableContainer";
 import translated from '../shared/Translated';
 
-class Customers extends React.Component {
 
-  state = {
-    data: null,
-    filters: {
-    }
-  }
+const Customers = (props) => {
 
-  loadData() {
-    this.props.customersFiltered(this.state.filters);
-  }
+  const [filters, setFilters] = useState({});
+  useEffect(() => {
+    dispatch(customersFiltered(filters));
+  }, []);
+  const dispatch = useDispatch();
+  const { data } = useSelector(({ listData }) => listData);
 
-  deleteSelected(data) {
-    this.props.deleteCustomer(data.id);
-  }
 
-  doFilter(data) {
-    this.setState({ filters: data })
-    this.props.customersFiltered(data);
-  }
-
-  render() {
-    return (
-      <TableContainer
-        title={translated('customer.title')}
-        data={this.props.data}
-        error={this.props.error}
-        onLoad={() => this.loadData()}
-        createView={"create-customer"}
-        updateView={"update-customer"}
-        readView={"customer"}
-        onDelete={(d) => this.deleteSelected(d)}
-        idBuilder={r => r.id}
-        isDeleting={this.props.isDeleting}
-        columns={[
-          { title: translated('customer.id'), field: "id" },
-          { title: translated('customer.referenceId'), field: "referenceId" },
-          { title: translated('customer.name'), field: "name" },
-          { title: translated('customer.email'), field: "email" },
-          { title: translated('customer.address1'), field: "address1" },
-          { title: translated('customer.address2'), field: "address2" },
-          { title: translated('customer.phone1'), field: "phone1" },
-          { title: translated('customer.phone2'), field: "phone2" }
-        ]}
-        filters={
-          [
-            { title: translated('customer.id'), field: "id", value: this.state.filters.id },
-            { title: translated('customer.referenceId'), field: "referenceId", value: this.state.filters.referenceId },
-            { title: translated('customer.name'), field: "name", value: this.state.filters.name }
-          ]
-        }
-        onFilter={(data) => this.doFilter(data)}
-      />
-    );
-  }
+  return (
+    <TableContainer
+      title={translated('customer.title')}
+      data={data}
+      onLoad={() => dispatch(customersFiltered(filters))}
+      createView={"create-customer"}
+      updateView={"update-customer"}
+      readView={"customer"}
+      onDelete={(d) => deleteCustomer(d.id)}
+      idBuilder={r => r.id}
+      isDeleting={false}
+      columns={[
+        { title: translated('customer.id'), field: "id" },
+        { title: translated('customer.referenceId'), field: "referenceId" },
+        { title: translated('customer.name'), field: "name" },
+        { title: translated('customer.email'), field: "email" },
+        { title: translated('customer.address1'), field: "address1" },
+        { title: translated('customer.address2'), field: "address2" },
+        { title: translated('customer.phone1'), field: "phone1" },
+        { title: translated('customer.phone2'), field: "phone2" }
+      ]}
+      filters={
+        [
+          { title: translated('customer.id'), field: "id", value: filters.id },
+          { title: translated('customer.referenceId'), field: "referenceId", value: filters.referenceId },
+          { title: translated('customer.name'), field: "name", value: filters.name }
+        ]
+      }
+      onFilter={(data) => setFilters(data)}
+    />
+  );
 }
 
-const mapStateToProps = ({ listData }) => {
-  return {
-    data: listData.data,
-    error: listData.error
-  };
-};
-
-export default connect(mapStateToProps, { customers, customersFiltered, deleteCustomer })(Customers);
+export default Customers;
--- src/components/shared/TableContainer.js
+++ src/components/shared/TableContainer.js
@@ -1,186 +1,172 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-import MaterialTable, { MTableToolbar } from "material-table";
-import { Button, Grid, Typography } from "@material-ui/core";
+import React, {useState} from "react";
+import MaterialTable, {MTableToolbar} from "material-table";
+import {Button, Grid, Typography} from "@material-ui/core";
 import CachedIcon from "@material-ui/icons/Cached";
 import Container from "@material-ui/core/Container";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
-import { withRouter } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
-import { formContainerStyles as styles } from "./Styles";
 import ErrorMessage from "./ErrorMessage";
 import AlertDialog from "./AlertDialog";
 import FilterContainer from "./FilterContainer";
 import translated from '../shared/Translated';
+import {useStyles} from './Styles';
+import {useSelector} from "react-redux";
 
-class TableContainer extends React.Component {
-
-  state = {
-    deleting: null
-  };
-
-  addErrorIfNeeded() {
-    if (this.props.error !== null)
-      return (
+const addErrorIfNeeded = (error) => {
+    return error && (
         <Container>
-          <ErrorMessage message={this.props.error} />
+            <ErrorMessage message={error}/>
         </Container>
-      );
-    return null;
-  }
-
-  addFiltersIfNeeded() {
-    if (!this.props.filters)
-      return;
-    return (
-      <FilterContainer
-        fields={this.props.filters}
-        onFilter={this.props.onFilter}
-      ></FilterContainer>
     );
-  }
+};
 
-  drawRefreshButtonIfNeeded(classes) {
-    if (!this.props.isChildView) {
-      return (<Button
-        color="default"
-        className={classes.button}
-        startIcon={<CachedIcon />}
-        onClick={() => {
-          this.props.onLoad();
-        }}
-      >
-        {translated('layout.refresh')}
-      </Button>);
+const addFiltersIfNeeded = (filters, onFilter) => {
+    return filters && (
+        <FilterContainer
+            fields={filters}
+            onFilter={onFilter}
+        />
+    );
+};
+
+const drawRefreshButtonIfNeeded = (classes, {isChildView,onLoad}) => {
+    if (!isChildView) {
+        return (<Button
+            color="default"
+            className={classes.button}
+            startIcon={<CachedIcon/>}
+            onClick={() => {
+                onLoad();
+            }}
+        >
+            {translated('layout.refresh')}
+        </Button>);
     }
-
-  }
-  render() {
-    const { classes } = this.props;
-    const dataToShow = this.props.data ? this.props.data : [];
-    return (
-      <React.Fragment>
-        <MaterialTable
-          title={""}
-          // other props
-          columns={this.props.columns}
-          data={dataToShow}
-          onRowClick={(event, rowData) =>
-            this.props.history.push(
-              `/${this.props.readView}/${this.props.idBuilder(rowData)}`
-            )
-          }
-          components={{
-            Toolbar: props => (
-              <React.Fragment>
-                <Grid justify="space-between" container>
-                  <Grid>
-                    <div className={classes.titleElement}>
-                      <Typography variant="h4" gutterBottom>
-                        {this.props.title}
-                      </Typography>
-                    </div>
-                  </Grid>
-                  <Grid>
-                    <div className={classes.titleElement}>
-                      <Button
-                        color="secondary"
-                        className={classes.button}
-                        startIcon={<AddCircleIcon />}
-                        onClick={() =>
-                          this.props.history.push(`/${this.props.createView}`)
-                        }
-                      >
-                        {translated('layout.add')}
-                      </Button>
-                      {this.drawRefreshButtonIfNeeded(classes)}
-                    </div>
-                  </Grid>
-                </Grid>
-                {this.addErrorIfNeeded()}
-                <Grid justify="space-between" container>
-                  <Grid>
-                    {this.addFiltersIfNeeded()}
-                  </Grid>
-                  <Grid>
-                    <MTableToolbar {...props} />
-                  </Grid>
-                </Grid>
-              </React.Fragment>
-            )
-          }}
-          actions={[
-            {
-              icon: "edit",
-              tooltip: "Update",
-              onClick: (event, rowData) =>
-                this.props.history.push(
-                  "/" +
-                  this.props.updateView +
-                  "/" +
-                  this.props.idBuilder(rowData)
-                )
-            },
-            {
-              icon: "delete",
-              tooltip: "delete",
-              onClick: (event, rowData) =>
-                this.setState({
-                  deleting: rowData
-                })
-            }
-          ]}
-
-          localization={{
-            pagination: {
-              labelDisplayedRows: '{from}-{to} of {count}'
-            },
-            toolbar: {
-              nRowsSelected: '{0} row(s) selected',
-              searchTooltip: translated('layout.search'),
-              searchPlaceholder: translated('layout.search'),
-            },
-            header: {
-              actions: translated('layout.actions')
-            },
-            body: {
-              emptyDataSourceMessage: translated('layout.empty'),
-              filterRow: {
-                filterTooltip: translated('layout.filter')
-              }
-            }
-          }}
-        />
-        {this.drawDeleteDialogIfNeeded()}
-      </React.Fragment>
-    );
-  }
-
-  handleDialogClose() {
-    this.setState({ deleting: null });
-  }
-
-  handleDialogAccept() {
-    this.props.onDelete(this.state.deleting);
-  }
-
-  handleDialogAcceptOk() {
-    this.setState({ deleting: null });
-    this.props.onLoad();
-  }
-
-  drawDeleteDialogIfNeeded() {
-    if (this.state.deleting != null)
-      return (
-        <AlertDialog
-          data={this.state.deleting}
-          handleClose={() => this.handleDialogClose()}
-          handleAccept={() => this.handleDialogAccept()}
-          handleAcceptOk={() => this.handleDialogAcceptOk()}
-        />
-      );
-    else return null;
-  }
 }
 
-export default withStyles(styles)(withRouter(TableContainer));
+
+const TableContainer = (props) => {
+
+    const [deleting, setDeleting] = useState(null);
+    const {error} = useSelector(({listData}) => listData);
+    const classes = useStyles();
+    const history = useHistory();
+
+    const dataToShow = props.data || [];
+
+    function handleDialogAcceptOk() {
+        setDeleting(null);
+        props.onLoad();
+    }
+
+    function drawDeleteDialogIfNeeded() {
+        if (deleting != null)
+            return (
+                <AlertDialog
+                    data={deleting}
+                    handleClose={() => setDeleting(null)}
+                    handleAccept={() => handleDialogAccept()}
+                    handleAcceptOk={() => handleDialogAcceptOk()}
+                />
+            );
+        else return null;
+    }
+
+    return (
+        <React.Fragment>
+            <MaterialTable
+                title={""}
+                // other props
+                columns={props.columns}
+                data={dataToShow}
+                onRowClick={(event, rowData) =>
+                    history.push(
+                        `/${props.readView}/${props.idBuilder(rowData)}`
+                    )
+                }
+                components={{
+                    Toolbar: props => (
+                        <React.Fragment>
+                            <Grid justify="space-between" container>
+                                <Grid>
+                                    <div className={classes.titleElement}>
+                                        <Typography variant="h4" gutterBottom>
+                                            {props.title}
+                                        </Typography>
+                                    </div>
+                                </Grid>
+                                <Grid>
+                                    <div className={classes.titleElement}>
+                                        <Button
+                                            color="secondary"
+                                            className={classes.button}
+                                            startIcon={<AddCircleIcon/>}
+                                            onClick={() =>
+                                                history.push(`/${props.createView}`)
+                                            }
+                                        >
+                                            {translated('layout.add')}
+                                        </Button>
+                                        {drawRefreshButtonIfNeeded(classes, props)}
+                                    </div>
+                                </Grid>
+                            </Grid>
+                            {addErrorIfNeeded()}
+                            <Grid justify="space-between" container>
+                                <Grid>
+                                    {addFiltersIfNeeded(props.filters, props.onFilter)}
+                                </Grid>
+                                <Grid>
+                                    <MTableToolbar {...props} />
+                                </Grid>
+                            </Grid>
+                        </React.Fragment>
+                    )
+                }}
+                actions={[
+                    {
+                        icon: "edit",
+                        tooltip: "Update",
+                        onClick: (event, rowData) =>
+                            history.push(
+                                "/" +
+                                props.updateView +
+                                "/" +
+                                props.idBuilder(rowData)
+                            )
+                    },
+                    {
+                        icon: "delete",
+                        tooltip: "delete",
+                        onClick: (event, rowData) =>
+                            setDeleting(rowData)
+                    }
+                ]}
+
+                localization={{
+                    pagination: {
+                        labelDisplayedRows: '{from}-{to} of {count}'
+                    },
+                    toolbar: {
+                        nRowsSelected: '{0} row(s) selected',
+                        searchTooltip: translated('layout.search'),
+                        searchPlaceholder: translated('layout.search'),
+                    },
+                    header: {
+                        actions: translated('layout.actions')
+                    },
+                    body: {
+                        emptyDataSourceMessage: translated('layout.empty'),
+                        filterRow: {
+                            filterTooltip: translated('layout.filter')
+                        }
+                    }
+                }}
+            />
+            {this.drawDeleteDialogIfNeeded()}
+        </React.Fragment>
+    );
+};
+
+export default TableContainer;
